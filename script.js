0e5e4fbd10cb495edfc599d73f55bfa514692309
zillomoons <44857214+zillomoons@users.noreply.github.com>
2020-06-08 13:39:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
 var date = new Date;
-	console.log(date);
+console.log(date);
 
-	let hr = date.getHours();
-	let min = date.getMinutes();
-	let sec = date.getSeconds();
-	console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
-	let hrPosition = (hr*360/12) + (min*(360/60)/12);
-	let minPosition = (min*360/60) + (sec*(360/60)/60);
-	let secPosition = sec*360/60;
+let hrPosition = (hr*360/12) + (min*(360/60)/12);
+let minPosition = (min*360/60) + (sec*(360/60)/60);
+let secPosition = sec*360/60;
 	
 function runTheClock() {	
 	
